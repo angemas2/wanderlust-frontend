2cb8d0a9cb9caa430b3add14c2663b4818ebeb13
Angélique <angelique.massolo@gmail.com>
2022-12-12 22:06:35 +0100

    nav improvment

---
+++ screens/ProfileScreen.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { SafeAreaView, Text, StyleSheet } from "react-native";
+
+export default function ProfileScreen() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Nav screen</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
