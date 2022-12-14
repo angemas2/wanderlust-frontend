6747a88734438adb87d698702b6ba69bc28a1c49
Lumpa12 <damien.thevenin@gate-16.com>
2022-12-14 12:43:38 +0100

    fix bug inscription

--- App.tsx
+++ App.tsx
@@ -3,8 +3,8 @@ import { useEffect, useState, useContext } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+
 import WelcomeScreen from "./screens/WelcomeScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
@@ -12,6 +12,7 @@ import ExploreScreen from "./screens/ExploreScreen";
 import InspirationScreen from "./screens/InspirationScreen";
 import MyTripsScreen from "./screens/MyTripsScreen";
 import NavScreen from "./screens/NavScreen";
+
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import {
   faMap,
@@ -20,12 +21,14 @@ import {
   faCompass,
   faXmark,
 } from "@fortawesome/free-solid-svg-icons";
-import { extendTheme, NativeBaseProvider } from "native-base";
 import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
+
+import { extendTheme, NativeBaseProvider } from "native-base";
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import * as Location from "expo-location";
 import PositionContext from "./utils/context";
+import places from "./reducers/places";
 
 import UserProvider, { UserContext } from "./utils/logincontext";
 
