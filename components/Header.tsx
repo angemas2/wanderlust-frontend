b80894ff56661c90883774eb4202a8f2dc807c69
Lumpa12 <damien.thevenin@gate-16.com>
2022-12-21 19:06:22 +0100

    fix header + intégration Header sur MyTripsScreen et InspirationScreen

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,13 @@
-import { useCallback } from "react";
-import {
-  SafeAreaView,
-  Text,
-  StyleSheet,
-  Image,
-  TouchableOpacity,
-  View,
-} from "react-native";
-import { useSelector, useDispatch } from "react-redux";
-import { UserState } from "../reducers/user";
-import { NavigationProp, ParamListBase } from "@react-navigation/native";
-import { Popover, Button, Box } from "native-base";
+import { useCallback } from 'react';
+import { SafeAreaView, Text, StyleSheet, Image, TouchableOpacity, View } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
+import { UserState } from '../reducers/user';
+import { NavigationProp, ParamListBase } from '@react-navigation/native';
+import { Popover, Button, Box } from 'native-base';
 
-import * as SplashScreen from "expo-splash-screen";
-import { useFonts, Inter_500Medium } from "@expo-google-fonts/dev"; //import fonts
-import { updateUserProfile } from "../reducers/user";
+import * as SplashScreen from 'expo-splash-screen';
+import { useFonts, Inter_500Medium } from '@expo-google-fonts/dev'; //import fonts
+import { updateUserProfile } from '../reducers/user';
 
 type HeaderProps = {
   navigation: NavigationProp<ParamListBase>;
@@ -22,13 +15,24 @@ type HeaderProps = {
 
 SplashScreen.preventAutoHideAsync();
 
-export default function Header({
-  navigation,
-  title,
-}: HeaderProps & { title: string }) {
+export default function Header({ navigation, title }: HeaderProps & { title: string }) {
   const user = useSelector((state: { user: UserState }) => state.user.value);
 
   const avatar = user.picture ? user.picture : '';
+
+  const dispatch = useDispatch();
+
+  const logout = () => {
+    dispatch(
+      updateUserProfile({
+        username: null,
+        email: null,
+        picture: null,
+        profile_id: null,
+      })
+    );
+    navigation.navigate('Login');
+  };
 
   const [fontsLoaded] = useFonts({
     Inter_500Medium,
@@ -44,63 +48,33 @@ export default function Header({
     return null;
   }
 
-  const dispatch = useDispatch();
-
-  const logout = () => {
-    dispatch(
-      updateUserProfile({
-        username: null,
-        email: null,
-        picture: null,
-        profile_id: null,
-      })
-    );
-    navigation.navigate("Login");
-  };
-
   return (
     <SafeAreaView style={styles.container} onLayout={onLayoutRootView}>
-      <Image
-        source={require("../assets/images/logo-header.png")}
-        style={styles.logo}
-      />
+      <Image source={require('../assets/images/logo-header.png')} style={styles.logo} />
       <Text style={styles.title}>{title}</Text>
       <View style={styles.userContainer}>
         <Popover
           trigger={(triggerProps) => {
             return (
-              <TouchableOpacity
-                onPress={() => navigation.navigate("Profile")}
-                {...triggerProps}
-              >
+              <TouchableOpacity onPress={() => navigation.navigate('Profile')} {...triggerProps}>
                 <Image style={styles.avatar} source={{ uri: avatar }} />
               </TouchableOpacity>
             );
-          }}
-        >
-          <Popover.Content
-            accessibilityLabel="Logout"
-            w="56"
-            style={{ top: 20 }}
-          >
+          }}>
+          <Popover.Content accessibilityLabel="Logout" w="56" style={{ top: 20 }}>
             <Popover.Arrow style={{ top: 25 }} />
             <Popover.CloseButton />
             <Popover.Header>
-              <Text style={{ fontWeight: "bold", fontSize: 12 }}>
-                Hi! 🖐 Welcome to Wanderlust
-              </Text>
+              <Text style={{ fontWeight: 'bold', fontSize: 12 }}>Hi! 🖐 Welcome to Wanderlust</Text>
             </Popover.Header>
             <Popover.Body
               style={{
-                display: "flex",
-                flexDirection: "row",
-              }}
-            >
+                display: 'flex',
+                flexDirection: 'row',
+              }}>
               <Image style={styles.avatar} source={{ uri: avatar }} />
               <View style={{ marginLeft: 10 }}>
-                <Text
-                  style={{ fontWeight: "bold", fontSize: 12, color: "#FFB703" }}
-                >
+                <Text style={{ fontWeight: 'bold', fontSize: 12, color: '#FFB703' }}>
                   {user.username}
                 </Text>
                 <Text style={{ fontSize: 10 }}>{user.email}</Text>
@@ -113,9 +87,8 @@ export default function Header({
                 </Button>
                 <Button
                   colorScheme="warning"
-                  style={{ backgroundColor: "#219EBC" }}
-                  onPress={() => logout()}
-                >
+                  style={{ backgroundColor: '#219EBC' }}
+                  onPress={() => logout()}>
                   Log out
                 </Button>
               </Button.Group>
@@ -131,31 +104,31 @@ export default function Header({
 
 const styles = StyleSheet.create({
   container: {
-    height: "10%",
-    width: "100%",
-    flexDirection: "row",
-    justifyContent: "space-evenly",
-    marginTop: "10%",
+    height: '10%',
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    marginTop: '10%',
   },
   logo: {
     aspectRatio: 1,
     width: 200,
-    right: "40%",
-    bottom: "35%",
+    right: '40%',
+    bottom: '35%',
   },
   title: {
-    fontFamily: "Inter_500Medium",
+    fontFamily: 'Inter_500Medium',
     fontSize: 20,
-    textAlign: "center",
-    right: "80%",
+    textAlign: 'center',
+    right: '80%',
     width: 150,
-    color: "#023047",
+    color: '#023047',
   },
   userContainer: {
-    display: "flex",
-    flexDirection: "column",
-    width: "18%",
-    height: "100%",
+    display: 'flex',
+    flexDirection: 'column',
+    width: '18%',
+    height: '100%',
   },
   avatar: {
     aspectRatio: 1,
@@ -164,9 +137,9 @@ const styles = StyleSheet.create({
   },
   username: {
     fontSize: 10,
-    textAlign: "center",
-    width: "70%",
-    color: "#023047",
+    textAlign: 'center',
+    width: '70%',
+    color: '#023047',
     opacity: 0.5,
   },
 });
